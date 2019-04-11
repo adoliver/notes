9a31f00d423bcfa3f979a8e0887e6adf50783e1b
Allen Oliver <AllenOliver@fico.com>
2019-04-11 14:18:53 -0700

    update

---
+++ stringSearch.py
@@ -0,0 +1,45 @@
+def runTest():
+	pass
+
+def hashChars(a,b):
+	if a > b:
+		return a+b
+	else:
+		return b+a
+	
+def findSubstring(input):
+	longest = "" #substring longest
+	current = "" #substring current
+
+	#dictionary of characters in current substring and last index position
+	charSet = {} 
+	maxCharSet = 2
+
+	# Loop over input characters
+	for c in input:
+		if len(charSet) 
+		current += c
+
+
+	if len(input) > 2:
+		firstChar = input[0]
+		secondChar = input[1]
+		longestSubstring = firstChar+lastChar
+		hashCurrent = hashChars(firstChar,secondChar)
+		hashLongest = hashCurrent
+		currentCount = 1
+		longestCount = currentCount
+		lastChar = firstChar
+		if lastChar != secondChar
+		for c in input[3:]:
+			h = hashChars(lastChar,c)
+			if h == hashCurrent:
+				currentCount = currentCount + 1
+			else:
+				
+				
+
+	else:
+		return input
+		
+		
